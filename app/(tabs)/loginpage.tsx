2f7f87de2e442dee07101ba36a13b075e112d196
dinhthanhnhat <dinhthanhnhat2k5@gmail.com>
2025-06-11 15:25:47 -0400

    basic login page

--- app/(tabs)/loginpage.tsx
+++ app/(tabs)/loginpage.tsx
@@ -1,19 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import React from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const loginpage = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [password, setPassword] = useState('')
     
   return (
-    <View>
-      <Text>loginpage</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>Login</Text>
+        <TextInput style={styles.input} placeholder='Email'/>
+        <TextInput style={styles.input} placeholder='Password' secureTextEntry={true}/>
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Login pressed')}>
+          <Text style={{textAlign: 'center', color: 'white'}}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => console.log('Register pressed')}>
+          <Text style={{textAlign: 'center', color: 'white'}}>Register</Text>
+        </TouchableOpacity>
+      </View>
+      
+    </SafeAreaView>
   )
 }
 
 export default loginpage
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  // formContainer: {
+  //   flex: 1,
+  //   // padding: 20,
+  //   justifyContent: 'center',
+  // },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 20,
+  },
+  input: {
+    height: 50,
+    width: '90%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  button: {
+    width: '90%',
+    backgroundColor: 'black',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginBottom: 15,
+  },
+  googleButton: {
+    width: '90%',
+    backgroundColor: 'black',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginBottom: 15,
+  }
 })
